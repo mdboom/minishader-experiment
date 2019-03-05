3e60a6e316b0c1debf40146712191e84e005f341
hamilton <hamilton.ulmer@gmail.com>
2019-03-04 21:09:31 -0800

    quick styling tweaks for demo

--- example.js
+++ example.js
@@ -3,27 +3,40 @@ function callback(data) {
     var slider = document.getElementById("range");
     slider.setAttribute("max", data.nframes - 1);
 
-    var background = document.getElementById("background");
-    background.setAttribute("height", data.height);
-    background.setAttribute("width", data.width);
-    background.setAttribute("style", `width: ${data.width / 2}px; height ${data.height / 2}px`);
+    // var background = document.getElementById("background");
+    // background.setAttribute("height", data.height);
+    // background.setAttribute("width", data.width);
+    // background.setAttribute("style", `width: ${data.width  * .8}px; height ${data.height * .8}px`);
 
-    var ctx = background.getContext("2d");
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, data.width, data.height);
+    // var ctx = background.getContext("2d");
+    // ctx.fillStyle = 'black';
+    // ctx.fillRect(0, 0, data.width, data.height);
 
     var canvas = document.getElementById("canvas");
     canvas.setAttribute("height", data.height);
     canvas.setAttribute("width", data.width);
-    canvas.setAttribute("style", `width: ${data.width / 2}px; height ${data.height / 2}px`);
+    canvas.setAttribute("style", `width: ${data.width * .8}px; height ${data.height * .8}px`);
 
     var outputBuffer = new Uint8ClampedArray(data.height * data.width * 4);
+    var clock = document.getElementById('clock')
+
+    const frameToClock = (fr) => {
+        //f * 5 * 60 // 12 intervals an hour * 24 hours
+        const totalMin = fr * 5
+        const hours = Math.floor(totalMin / (60 * 5))
+        const mins = Math.floor(totalMin % (60 * 5)) / 5
+        return `${(hours < 10 ? '0' : '') + hours}:${(mins < 10 ? '0' : '') + mins}:00`
+    }
 
     slider.oninput = function() {
         var frame = decompressFrame(data, this.value, outputBuffer, inferno);
         var imageData = new ImageData(outputBuffer, data["width"], data["height"]);
         var ctx = canvas.getContext("2d");
         ctx.putImageData(imageData, 0, 0);
+        // update clock.
+        clock.innerHTML = frameToClock(this.value)
+        //console.log(this.value);
+
     }
     slider.value = 0;
     slider.oninput();
@@ -32,11 +45,11 @@ function callback(data) {
     var playTimerId;
     button.onclick = function() {
         if (playTimerId === undefined) {
-            button.innerHTML = "⏹";
+            button.innerHTML = "&block;";
             playTimerId = window.setInterval(function() {
                 slider.value = parseInt(slider.value) + 1;
                 slider.oninput();
-            }, 50);
+            }, 30);
         } else {
             button.innerHTML = "▶";
             window.clearInterval(playTimerId);
