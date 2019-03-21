4bbab043ac26521e4302cd995e5280afb5d4a11d
hamilton <hamilton.ulmer@gmail.com>
2019-03-20 23:43:03 -0700

    tests

--- src/validate-arguments.js
+++ src/validate-arguments.js
@@ -6,6 +6,7 @@ export const DEFAULT_ARGUMENTS = {
 export const REQUIRED_FIELDS = new Set(['target', 'data'])
 
 const validateArguments = (args) => {
+    if (args === undefined || typeof args !== 'object') throw new Error('args must be an object')
     const keys = Object.keys(args)
     REQUIRED_FIELDS.forEach((k) => {
         if (!keys.includes(k)) throw new Error(`missing required argument ${k}`)
